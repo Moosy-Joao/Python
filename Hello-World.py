38d2df68d62e0b2305e6b2cd69fcbd88a050aa67
Joao Pedro <Jpp15marques@gmail.com>
2025-03-13 00:18:53 -0300

    Exercicios adicionados

---
+++ Hello-World.py
@@ -0,0 +1,54 @@
+print("Olá, Mundo!")
+
+nome= "João"
+idade= 20
+altura=1.80
+
+print(nome)
+print(idade)
+print(altura)
+
+a=10
+b=20
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+if_idade= 20
+
+if if_idade >=18:
+    print("voce é maior de idade!")
+else:
+    print("voce é menor de idade!")
+    
+    i = 0
+    for i in range(5):
+        print(i)
+      
+
+    while contador < 5:
+        contador = 0
+    print(contador)
+    contador += 1
+    
+    def saudacao(nome):
+        print(f"Olá, {nome}!")
+
+    saudacao("Maria")
+    saudacao("José")
+    
+    
+    frutas = ["maçã", "banana", "laranja"] # Cria uma lista de frutas
+print(frutas[0])  # Acessa o primeiro item da lista
+
+frutas.append("uva")  # Adiciona um item à lista
+print(frutas)
+
+frutas.remove("banana")  # Remove um item da lista
+print(frutas)
+
+import math
+
+print(math.sqrt(16))  # Raiz quadrada de 16
